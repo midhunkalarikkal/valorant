e6d11c24805d100c7bb4947e930011f18b29ad6a
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2023-11-30 23:51:47 +0530

    neccessary packages installed and sample app build

---
+++ main.js
@@ -0,0 +1,50 @@
+require("dotenv").config()
+const express = require('express')
+const session = require('express-session')
+const mongoose = require('mongoose')
+const MongoStore = require("connect-mongo")
+
+const app = express()
+const PORT = process.env.PORT || 7000
+
+//databse connection
+mongoose.connect(process.env.DB_URI)
+const db = mongoose.connection
+db.on("error" , (error) => console.log(err , "Db connection error"))
+db.once("open" , () => console.log("Database connected"))
+
+//view engine
+app.set("view engine","ejs")
+
+//middlewares
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+
+
+app.use("",require("./routes/router"))
+
+
+//session
+app.use(session({
+    secret : "my secret key",
+    saveUninitialized : false,
+    resave : false,
+    store: MongoStore.create({ mongoUrl:process.env.DB_URI }),
+    cookie:{
+        name:'myCookie',
+        maxAge:1000*60*60*2,
+        sameSite: true,
+    }
+}))
+
+
+app.use((req,res,next)=>{
+    res.locals.message = req.session.message
+    delete req.session.message
+    next()
+})
+
+
+app.listen(PORT, ()=> { 
+    console.log(`Server is listening to the port ${PORT}`)
+})
