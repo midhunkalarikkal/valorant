047fb87ef37b577e1feadb84f3e985860c4331da
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2023-12-01 12:55:20 +0530

    admin ejs pages added

--- routes/router.js
+++ routes/router.js
@@ -9,7 +9,22 @@ router.get('/', (req, res) => {
 
 //route to get the user register page
 router.get('/register', (req, res) => {
-    res.render('user_register', { title: "User Login", message: '', errmsg: "" })
+    res.render('user_register', { title: "User Register", message: '', errmsg: "" })
+})
+
+//route to get the home page
+router.get('/home', (req, res) => {
+    res.render('home', { title: "Home Page"})
+})
+
+//route to get the admin login
+router.get('/admin', (req, res) => {
+    res.render('admin_login', { title: "Admin Login", message: '', errmsg: "" })
+})
+
+//route to get the admin dashboard
+router.get('/admin_dashboard', (req, res) => {
+    res.render('admin_dashboard', { title: "Admin Dashboard"})
 })
 
 module.exports = router
