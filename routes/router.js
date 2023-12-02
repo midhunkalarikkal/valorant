f51ceb09cb0add6c983d6ccf8688b1fd06343283
midhun kalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2023-12-02 22:59:25 +0530

    bcrypt package installed and password hashing is added , user login , user registration , admin login page design edited

--- routes/router.js
+++ routes/router.js
@@ -6,6 +6,7 @@ const User = require('../models/users')
 const multer = require('multer')
 const fs = require('fs')
 const path = require('path')
+const bcrypt = require('bcrypt')
 
 router.use(session({
     secret: "my secret key",
@@ -19,7 +20,7 @@ router.use(session({
     }
 }))
 
-router.use((req,res,next)=>{
+router.use((req, res, next) => {
     res.locals.message = req.session.message
     delete req.session.message
     next()
@@ -84,12 +85,14 @@ router.post("/register", upload, async (req, res) => {
             return res.render("user_register", { title: "User Register", message: "", errmsg: "Email already in use." })
         }
 
+        const hashpassword = await bcrypt.hash(req.body.password, 10)
+
         const user = new User({
             name: req.body.name,
             email: req.body.email,
             phone: req.body.phone,
             image: req.file.filename,
-            password: req.body.password
+            password: hashpassword
         })
         await user.save()
         res.render("user_login", { title: "User Login", message: "Successfull registration", errmsg: "" });
@@ -101,19 +104,24 @@ router.post("/register", upload, async (req, res) => {
 
 //route to check login data in database and redirecting to home page
 router.post('/', async (req, res) => {
-    const user = await User.findOne({ email: req.body.email });
-    const password = req.body.password
-    if (!user) {
-        return res.render('user_login', { title: "User Login", message: "", errmsg: "No user found" });
-    }
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        const password = req.body.password
+        if (!user) {
+            return res.render('user_login', { title: "User Login", message: "", errmsg: "No user found" });
+        }
 
-    if (password !== user.password) {
-        return res.render('user_login', { title: "User Login", message: "", errmsg: "Wrong password" });
-    }
+        const ismatch = await bcrypt.compare(password, user.password)
+        if (!ismatch) {
+            return res.render('user_login', { title: "User Login", message: "", errmsg: "Wrong password" });
+        }
 
-    if (req.body.password === user.password && req.body.email === user.email) {
-        req.session.user = user;
-        res.redirect("/home")
+        if (ismatch && req.body.email === user.email) {
+            req.session.user = user;
+            res.redirect("/home")
+        }
+    } catch (error) {
+        return res.render('user_login', { title: "User Login", message: "", errmsg: "Internal error" });
     }
 
 });
@@ -164,7 +172,7 @@ router.get('/admin_dashboard', async (req, res) => {
         if (req.session.admin) {
             User.find().exec()
                 .then((users) => {
-                    res.render('admin_dashboard', { title: "Admin Dashboard", users : users});
+                    res.render('admin_dashboard', { title: "Admin Dashboard", users: users });
                 })
                 .catch((err) => {
                     res.json({ message: err.message })
@@ -198,136 +206,138 @@ router.get('/admin-logout', async (req, res) => {
 });
 
 //route to get user adding page
-router.get('/add-user', async (req,res)=>{
-    try{
-        if(req.session.admin){
-            res.render("admin_add_user",{title : "Admin add User"})
-        }else{
+router.get('/add-user', async (req, res) => {
+    try {
+        if (req.session.admin) {
+            res.render("admin_add_user", { title: "Admin add User" })
+        } else {
             res.render('admin_login', { title: "Admin Login", message: "", errmsg: "Relogin needed" });
         }
-    }catch(err){
+    } catch (err) {
         console.error("Error in admin-add-user route:", error);
         res.status(500).send("Internal Server Error");
     }
 })
 
 //route to post new user data to databse
-router.post('/add', upload , async (req,res)=>{
-    try{
-    const user = new User({
-        name : req.body.name,
-        email : req.body.email,
-        phone : req.body.phone,
-        password : req.body.password,
-        image : req.file.filename,
-    })
-    await user.save()
-            req.session.message = {
-                type : "success",
-                message : "User added successfully.."
-            }
-            res.redirect("/admin_dashboard")
-        }catch(error){
+router.post('/add', upload, async (req, res) => {
+    try {
+        //Hashing the password
+        const hpass = await bcrypt.hash(req.body.password, 10)
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: hpass,
+            image: req.file.filename,
+        })
+        await user.save()
+        req.session.message = {
+            type: "success",
+            message: "User added successfully.."
+        }
+        res.redirect("/admin_dashboard")
+    } catch (error) {
         console.error("Error in admin-add-user route:", error);
         res.status(500).send("Internal Server Error");
     }
 })
 
 //route to get the admin user edit page
-router.get('/edit/:id', (req,res)=>{
+router.get('/edit/:id', (req, res) => {
     let id = req.params.id
     User.findById(id)
-    .then((user)=>{
-        if(user == null){
+        .then((user) => {
+            if (user == null) {
                 res.redirect('/admin_dashboard')
-        }else{
-            res.render("admin_edit_user",{title : "Admin Edit User", user : user})
-        }
-    })
-    .catch((err) => {
-        res.redirect('/admin_dashboard')
-    })
+            } else {
+                res.render("admin_edit_user", { title: "Admin Edit User", user: user })
+            }
+        })
+        .catch((err) => {
+            res.redirect('/admin_dashboard')
+        })
 })
 
 //route to post the admin user edit / update data in to database
-router.post('/update/:id', upload , (req,res)=>{
+router.post('/update/:id', upload, (req, res) => {
     const id = req.params.id
     let new_img = ""
 
-    if(req.file){
+    if (req.file) {
         new_img = req.file.filename
-        try{
-            fs.unlinkSync("./uploads/"+req.body.old_image)
-        }catch(err){
+        try {
+            fs.unlinkSync("./uploads/" + req.body.old_image)
+        } catch (err) {
             console.error('Error deleting old image:', err);
             return res.status(500).send("Internal Server Error");
         }
-    }else{
+    } else {
         new_img = req.body.old_image
     }
 
-    User.findByIdAndUpdate(id , {
-        name : req.body.name,
-        email : req.body.email,
-        phone : req.body.phone,
-        password : req.body.password,
-        image : new_img,
+    User.findByIdAndUpdate(id, {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        // password : req.body.password,
+        image: new_img,
     })
-    .then(()=>{
-        if(req.session.admin){
-            req.session.message = {
-                type : "success",
-                message : "User updated Successfully.."
+        .then(() => {
+            if (req.session.admin) {
+                req.session.message = {
+                    type: "success",
+                    message: "User updated Successfully.."
+                }
+                res.redirect('/admin_dashboard')
+            } else {
+                res.render("admin_login", { titlle: "Admin Login", message: "", errmsg: "Relogin needed" })
             }
-            res.redirect('/admin_dashboard')
-        }else{
-            res.render("admin_login",{titlle : "Admin Login" , message : "", errmsg : "Relogin needed"})
-        }
-    })
-    .catch((err)=>{
-        if(req.session.admin){
-            req.session.message = {
-                type : "danger",
-                message : "Error in user updation!"
+        })
+        .catch((err) => {
+            if (req.session.admin) {
+                req.session.message = {
+                    type: "danger",
+                    message: "Error in user updation!"
+                }
+                res.redirect('/admin_dashboard')
+                console.log(err)
+            } else {
+                res.render("admin_login", { titlle: "Admin Login", message: "", errmsg: "Rwlogin needed" })
             }
-            res.redirect('/admin_dashboard')
-            console.log(err)
-        }else{
-            res.render("admin_login",{titlle : "Admin Login" , message : "", errmsg : "Rwlogin needed"})
-        }
-    })
+        })
 })
 
 //route to get the admin user delete
-router.get('/delete/:id',(req,res)=>{
+router.get('/delete/:id', (req, res) => {
     let id = req.params.id
     User.findByIdAndDelete(id)
-    .then((result)=>{
-        if(result.image && result.image != ""){
-            try{
-                fs.unlinkSync("./uploads/"+result.image)
-            }catch(err){
-                res.status(500).send(err.message);
+        .then((result) => {
+            if (result.image && result.image != "") {
+                try {
+                    fs.unlinkSync("./uploads/" + result.image)
+                } catch (err) {
+                    res.status(500).send(err.message);
+                }
             }
-        }
             req.session.message = {
-                type : "success",
-                message : "User deleted successfuly.."
+                type: "success",
+                message: "User deleted successfuly.."
             }
             res.redirect('/admin_dashboard')
-    })
-    .catch((err)=>{
-        if(req.session.admin){
-            req.session.message = {
-                type : "danger",
-                message : "Error in user deletion!"
+        })
+        .catch((err) => {
+            if (req.session.admin) {
+                req.session.message = {
+                    type: "danger",
+                    message: "Error in user deletion!"
+                }
+                res.redirect('/admin_dashboard')
+                console.log("Error in user deleting ", err)
+            } else {
+                res.render("admin_login", { titlle: "Admin Login", message: "", errmsg: "Rwlogin needed" })
             }
-            res.redirect('/admin_dashboard')
-            console.log("Error in user deleting ",err)
-        }else{
-            res.render("admin_login",{titlle : "Admin Login" , message : "", errmsg : "Rwlogin needed"})
-        }
-    })
+        })
 })
 
 module.exports = router
